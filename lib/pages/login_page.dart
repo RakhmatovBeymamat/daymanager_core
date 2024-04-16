d214ff262974365485c584ba3c818a3bd389ac20
Beymamat Rakhmatov <beymamatrakhmatov@MacBook-Air-Beymamat.local>
2024-04-16 16:10:49 +0500

    init auth view

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,76 +1,157 @@
-import 'package:daymanager_core/components/dmtextfield.dart';
 import 'package:flutter/material.dart';
+import 'package:daymanager_core/components/my_button.dart';
+import 'package:daymanager_core/components/my_textfield.dart';
+import 'package:daymanager_core/components/square_tile.dart';
 
 class LoginPage extends StatelessWidget {
   LoginPage({super.key});
 
+  // text editing controllers
   final usernameController = TextEditingController();
   final passwordController = TextEditingController();
+
+  // sign user in method
+  void signUserIn() {}
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[300],
       body: SafeArea(
-          child: Center(
-        child: Column(
-          children: [
-            const SizedBox(height: 50),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const SizedBox(height: 50),
 
-            //logo
-            const Icon(
-              Icons.lock,
-              size: 100,
-            ),
-
-            const SizedBox(height: 30),
-            //title
-
-            const Text(
-              "Добро пожаловать!",
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 32,
-                fontWeight: FontWeight.bold,
+              // logo
+              const Icon(
+                Icons.lock,
+                size: 100,
               ),
-            ),
 
-            // const SizedBox(height: 25),
+              const SizedBox(height: 50),
 
-            Container(
-              width: 400,
-              padding: const EdgeInsets.all(20),
-              child: const Column(
-                children: [
-                  //username textfield
-                  DMTextField(
-                    controller: usernameController,
-                    hintText: "",
-                    obsecureText: false,
-                  ),
+              // welcome back, you've been missed!
+              Text(
+                'Welcome back you\'ve been missed!',
+                style: TextStyle(
+                  color: Colors.grey[700],
+                  fontSize: 16,
+                ),
+              ),
 
-                  SizedBox(height: 15),
+              const SizedBox(height: 25),
 
-                  //password textfield
-                  DMTextField(
-                    controller: passwordController,
-                    hintText: "",
-                    obsecureText: true,
-                  ),
+              // username textfield
+              MyTextField(
+                controller: usernameController,
+                hintText: 'Username',
+                obscureText: false,
+              ),
+
+              const SizedBox(height: 10),
+
+              // password textfield
+              MyTextField(
+                controller: passwordController,
+                hintText: 'Password',
+                obscureText: true,
+              ),
+
+              const SizedBox(height: 10),
+
+              // forgot password?
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text(
+                      'Forgot Password?',
+                      style: TextStyle(color: Colors.grey[600]),
+                    ),
+                  ],
+                ),
+              ),
+
+              const SizedBox(height: 25),
+
+              // sign in button
+              MyButton(
+                onTap: signUserIn,
+              ),
+
+              const SizedBox(height: 50),
+
+              // or continue with
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Divider(
+                        thickness: 0.5,
+                        color: Colors.grey[400],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                      child: Text(
+                        'Or continue with',
+                        style: TextStyle(color: Colors.grey[700]),
+                      ),
+                    ),
+                    Expanded(
+                      child: Divider(
+                        thickness: 0.5,
+                        color: Colors.grey[400],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+
+              const SizedBox(height: 50),
+
+              // google + apple sign in buttons
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  // google button
+                  SquareTile(imagePath: 'lib/images/google.png'),
+
+                  SizedBox(width: 25),
+
+                  // apple button
+                  SquareTile(imagePath: 'lib/images/apple.png')
                 ],
               ),
-            )
 
-            //forgot password
+              const SizedBox(height: 50),
 
-            //sign in btn
-
-            // google
-
-            //register
-          ],
+              // not a member? register now
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Not a member?',
+                    style: TextStyle(color: Colors.grey[700]),
+                  ),
+                  const SizedBox(width: 4),
+                  const Text(
+                    'Register now',
+                    style: TextStyle(
+                      color: Colors.blue,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
         ),
-      )),
+      ),
     );
   }
 }
